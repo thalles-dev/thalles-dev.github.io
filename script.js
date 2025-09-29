1aef35931231e187efc9d0df0e8e4c849f022fa6
thalles-dev <thallyslira@gmail.com>
2025-09-28 22:37:09 -0300

    naoseimais

--- script.js
+++ script.js
@@ -14,8 +14,7 @@ const msgII = document.getElementById("msg-ii");
 // Função: muda layout pela hora
 // =========================
 function carregar() {
-  //const hora = new Date().getHours();
-  hora = 3
+  const hora = new Date().getHours();
   if (hora >= 0 && hora < 6) {
     imagemHora.src = 'images/madrugada.gif';
     document.body.style.background = '#000102';
